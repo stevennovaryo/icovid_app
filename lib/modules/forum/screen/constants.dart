a190c70a0fc08ecbf4bc19b78b10e152bcf64811
gbhisma <bhismagregorius@gmail.com>
2021-12-31 10:52:21 +0700

    [forum] functional forum home

---
+++ lib/modules/forum/screen/constants.dart
@@ -0,0 +1,3 @@
+import 'package:icovid_app/core/services/constants.dart';
+
+const String URL_FORUM_LIST = '$BACKEND_HOST/forum/api/ForumHome/';
--- lib/modules/forum/screen/forumHome.dart
+++ lib/modules/forum/screen/forumHome.dart
@@ -1,7 +1,6 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:icovid_app/modules/forum/services/services.dart';
 
 class ForumHome extends StatefulWidget {
     const ForumHome({Key? key}) : super(key: key);
@@ -10,18 +9,44 @@ class ForumHome extends StatefulWidget {
     State<ForumHome> createState() => _ForumHomeState();
 }
 class _ForumHomeState extends State<ForumHome> {
-    @override
+  List<ForumPost> _forumList = List.empty();
+  List<Widget> _forumThumbList = List.empty();
+  
+
+  Future _getForumList() async {
+    dynamic response = await fetchForumList();
+    List<ForumPost> _tmp_list = response;
+    List <Widget> _tmp = [];
+    for (var i = 0; i < _tmp_list.length; i++) {
+      _tmp.add(thumbify(_tmp_list[i]));
+    }
+    setState(() {
+      _forumList = _tmp_list;
+      _forumThumbList = _tmp;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getForumList();
+  }
+
+  @override
 	Widget build(BuildContext context) {
 		return Scaffold(
 			appBar: AppBar(title: const Text("ICovid - Forum")),
 			body: Column(
 				crossAxisAlignment: CrossAxisAlignment.stretch,
-				children: [
-					thumbify(ForumPost("Greg", "Hai, dunia! 1", "Ngetest", "10-11-2021")),
-					thumbify(ForumPost("Or", "Hai, dunia! 2", "Ngetest", "10-11-2021")),
-					thumbify(ForumPost("Ius", "Hai, dunia! 3", "Ngetest", "10-11-2021")),
-					thumbify(ForumPost("Bhis", "Hai, dunia! 4", "Ngetest", "10-11-2021")),
-				],
+				children: 
+        // [
+				// 	thumbify(ForumPost(author: "Greg", topic: "Hai, dunia! 1", description: "Ngetest", date_created: "10-11-2021", sl"hai-1" )),
+				// 	thumbify(ForumPost("Or", "Hai, dunia! 2", "Ngetest", "10-11-2021", "hai-2")),
+				// 	thumbify(ForumPost("Ius", "Hai, dunia! 3", "Ngetest", "10-11-2021")),
+				// 	thumbify(ForumPost("Bhis", "Hai, dunia! 4", "Ngetest", "10-11-2021")),
+				// ]
+        _forumThumbList,
 			),
 		);
 	}
@@ -46,13 +71,4 @@ class _ForumHomeState extends State<ForumHome> {
 			),
 		);
 	}
-}
-
-class ForumPost {
-	String author;
-	String topic;
-	String description;
-	String dateCreated;
-
-	ForumPost(this.author, this.topic, this.description, this.dateCreated);
 }
